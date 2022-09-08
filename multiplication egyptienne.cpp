88b8dd611a37800edd965b3d937130a5711f23d4
Bouzhar <abddoullah.bouzhar@gmail.com>
2022-09-08 09:47:58 +0100

    commit initial M/E

---
+++ multiplication egyptienne.cpp
@@ -0,0 +1,18 @@
+ #include <math.h> 
+ #include <stdlib.h>
+ int i,n,a,b,c;
+int main() {
+	start:
+		
+ printf("Enter an integer\n");
+	scanf("%d", &n);
+  for(i=1;i<=n;i++) {
+    ;
+  }
+printf("%d",u);
+printf("\n");
+u=6;
+goto start;
+system("CLS");
+  return 0;
+}
